2a50d078d5b9a91010918ca4134bdeb62ecd32d9
IliaHF <imhflor.code@gmail.com>
2023-09-13 15:43:34 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'app') },
+    ],
+  },
   root: "app",
   server: {
     port: 8080,
